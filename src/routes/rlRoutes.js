7e777d3efcb901f5711d4f3e287533b525dc7809
Lakshay Shrivastav <lakshay@example.com>
2026-03-12 14:20:26 +0530

    Fixed middleware path in rlRoutes

--- src/routes/rlRoutes.js
+++ src/routes/rlRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const rlController = require("../controllers/rlController");
-const protect = require("../middleware/protect");
+const protect = require("../middleware/protectedRoutes");
 
 router.post("/update", protect, rlController.updateReward);
 
